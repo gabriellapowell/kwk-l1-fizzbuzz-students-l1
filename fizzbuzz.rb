2ee32eaf45e5e7d0e55aae6186d098b50b7ee8f7
gabriellapowell <soccerplayer60@icloud.com>
2018-06-06 20:04:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(int)
   
 if int % 3==0 && int % 5==0
-   "FizzBuzz"
+  puts "FizzBuzz"
   elsif int % 3==0 
-   "Fizz"
+   puts "Fizz"
   elsif int % 5==0 
-   "Buzz"
+   puts "Buzz"
 else
-   "nil"
+   puts "nil"
 end
 end
 
